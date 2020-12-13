35ecd5f0fc6eca4e0dfd99a4d8bd6a8346d43963
collaborator-kmn <68353619+collaborator-kmn@users.noreply.github.com>
2020-12-13 11:49:10 +0300

    static default

--- src/main/java/kmn/marduk/db/MardukConnection.java
+++ src/main/java/kmn/marduk/db/MardukConnection.java
@@ -1,5 +1,7 @@
 package kmn.marduk.db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.util.Optional;
@@ -7,39 +9,39 @@ import java.util.Properties;
 
 public class MardukConnection implements Connector<Connection> {
 
-    private Properties mardukProperties;
-    private InputStream inputStream;
+    private final Properties mardukProperties;
 
     public MardukConnection(Properties properties) {
         this.mardukProperties = properties;
     }
 
-    public MardukConnection() {
-        this.inputStream = getClass().getClassLoader().getResourceAsStream("marduk.properties");
-    }
 
-    private Optional<Properties> defaultProperties() {
+    private static Optional<Properties> defaultProperties(InputStream inputStream) {
         try {
             Properties properties = new Properties();
             properties.load(inputStream);
-            this.mardukProperties = properties;
-            return Optional.of(mardukProperties);
+            return Optional.of(properties);
         } catch (Exception e) {
             System.out.println("Error read file marduk.prop. " + e);
             return Optional.empty();
         }
     }
 
+    private static Optional<MardukConnection> defaultConnection(String properties) throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream(properties);
+        Optional<Properties> perhaps = defaultProperties(inputStream);
+
+        return perhaps.map(MardukConnection::new);
+    }
+
     @Override
     public Connection connect() throws ConnectorException {
-        if (defaultProperties().isPresent()) {
-            OracleHandle oracleHandle = new OracleHandle();
-            oracleHandle.setServer(mardukProperties.getProperty("server"));
-            oracleHandle.setPassword(mardukProperties.getProperty("password").toCharArray());
-            oracleHandle.setDatabase(mardukProperties.getProperty("database"));
-            oracleHandle.setPort(Integer.parseInt(mardukProperties.getProperty("port")));
+        OracleHandle oracleHandle = new OracleHandle();
+        oracleHandle.setServer(mardukProperties.getProperty("server"));
+        oracleHandle.setPassword(mardukProperties.getProperty("password").toCharArray());
+        oracleHandle.setDatabase(mardukProperties.getProperty("database"));
+        oracleHandle.setPort(Integer.parseInt(mardukProperties.getProperty("port")));
 
-            return new JDBCConnector(oracleHandle).connect();
-        } else return null;
+        return new JDBCConnector(oracleHandle).connect();
     }
 }
